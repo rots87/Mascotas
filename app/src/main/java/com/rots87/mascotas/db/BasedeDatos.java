83ed1ef33b1bd60d7bd26ed7520e37d7d0396838
Nestor Cañas <ncanas@openmailbox.org>
2016-10-14 00:28:56 -0600

    Primeras lineas de la base de datos

---
+++ app/src/main/java/com/rots87/mascotas/db/BasedeDatos.java
@@ -0,0 +1,8 @@
+package com.rots87.mascotas.db;
+
+/**
+ * Created by rots on 10-13-16.
+ */
+
+public class BasedeDatos {
+}
---
+++ app/src/main/java/com/rots87/mascotas/db/ConstantesDB.java
@@ -0,0 +1,8 @@
+package com.rots87.mascotas.db;
+
+/**
+ * Created by rots on 10-14-16.
+ */
+
+public class ConstantesDB {
+}
