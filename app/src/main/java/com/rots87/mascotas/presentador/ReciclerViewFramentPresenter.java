9a3d7d1977b2e37e8ed9576ec1b6413211878912
Nestor Cañas <ncanas@openmailbox.org>
2016-10-31 22:58:25 -0600

    Terminando el presentador pero aun no prestan los datos consumidos del GSON

--- app/src/main/java/com/rots87/mascotas/presentador/ReciclerViewFramentPresenter.java
+++ app/src/main/java/com/rots87/mascotas/presentador/ReciclerViewFramentPresenter.java
@@ -1,11 +1,21 @@
 package com.rots87.mascotas.presentador;
 
 import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.rots87.mascotas.db.ConstructorMascotas;
 import com.rots87.mascotas.fragment.RecyclerViewFragmentView;
 import com.rots87.mascotas.Pojo.mascotas;
+import com.rots87.mascotas.restApi.Adapter.RestApiAdapter;
+import com.rots87.mascotas.restApi.EndpointApi;
+import com.rots87.mascotas.restApi.model.MascotasResponse;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by rots on 10-15-16.
@@ -34,7 +44,25 @@ public class ReciclerViewFramentPresenter implements IRecyclerViewFragmentPresen
 
     @Override
     public void obtenerMediosRecientes() {
-        
+        RestApiAdapter restApiAdapter = new RestApiAdapter();
+        EndpointApi endpointApi = restApiAdapter.establecerConexionRestApiInstagram();
+
+        Call<MascotasResponse> mascotasResponseCall = endpointApi.getRecentMedia();
+        mascotasResponseCall.enqueue(new Callback<MascotasResponse>() {
+            @Override
+            public void onResponse(Call<MascotasResponse> call, Response<MascotasResponse> response) {
+                MascotasResponse mascotasResponse = response.body();
+                mascota = mascotasResponse.getMascota();
+                mostrarMascotasRV();
+
+            }
+
+            @Override
+            public void onFailure(Call<MascotasResponse> call, Throwable t) {
+                Toast.makeText(context,"Error, intenta de nuevo",Toast.LENGTH_LONG);
+                Log.e("Error",t.toString());
+            }
+        });
     }
 
     @Override
